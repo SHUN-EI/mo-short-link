a5aaa833ff2de6078512b5d144c84eb7b34d9d61
moweiquan <295597613@qq.com>
2022-02-28 23:22:37 +0800

    mo-short-link-订单服务-项目基础骨架搭建，以及相关sql脚本

---
+++ mocloud-shop/src/main/java/com/mo/mapper/ProductOrderMapper.java
@@ -0,0 +1,16 @@
+package com.mo.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.mo.model.ProductOrderDO;
+
+/**
+ * <p>
+ * 订单表_0 Mapper 接口
+ * </p>
+ *
+ * @author mo
+ * @since 2022-02-28
+ */
+public interface ProductOrderMapper extends BaseMapper<ProductOrderDO> {
+
+}
