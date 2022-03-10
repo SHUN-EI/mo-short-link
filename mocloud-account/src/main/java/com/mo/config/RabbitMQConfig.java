45f6d47b9361575501d3d94bcabb012d53e88e0f
moweiquan <295597613@qq.com>
2022-03-10 16:18:54 +0800

    mo-short-link-账号服务-增加MQ队列和异常MQ队列相关配置

---
+++ mocloud-account/src/main/java/com/mo/config/RabbitMQConfig.java
@@ -0,0 +1,27 @@
+package com.mo.config;
+
+import lombok.Data;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by mo on 2022/3/10
+ */
+@Configuration
+@Data
+public class RabbitMQConfig {
+
+    /**
+     * 消息转换器，把消息转为json格式
+     *
+     * @return
+     */
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+
+}
